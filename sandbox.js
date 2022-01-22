f84d8c1680e98ef018a23fcc4e509605e270d161
Edwin Boon <edwin.boon@eo.nl>
2022-01-22 10:41:48 +0100

    Add ts file and practice with tsc

--- sandbox.js
+++ sandbox.js
@@ -1,1 +1,7 @@
-// JS code here
+var character = 'Mario';
+console.log({ character: character });
+var inputs = document.querySelectorAll('input');
+console.log({ inputs: inputs });
+inputs.forEach(function (input) {
+    console.log({ input: input });
+});
