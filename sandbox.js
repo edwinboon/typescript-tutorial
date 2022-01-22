68b4d9affe2498b86a1507e80c5ee3aa608f7171
Edwin Boon <edwin.boon@eo.nl>
2022-01-22 15:56:55 +0100

    New folder setup and tsconfig

--- sandbox.js
+++
@@ -1,7 +0,0 @@
-var character = 'Mario';
-console.log({ character: character });
-var inputs = document.querySelectorAll('input');
-console.log({ inputs: inputs });
-inputs.forEach(function (input) {
-    console.log({ input: input });
-});
